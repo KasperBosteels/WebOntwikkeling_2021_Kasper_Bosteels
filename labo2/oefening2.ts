b4e2c9bcd649d64a1c33a0612e7c6f3fba391ad3
KasperBosteels <kasperbosteels@hotmail.com>
2021-03-03 11:50:08 +0100

    oefening 2 gedaan

---
+++ labo2/oefening2.ts
@@ -0,0 +1,9 @@
+enum regenboog {"rood","oranje","geel","groen","lichtblauw","donkerblauw"};
+let aray = [];
+for (let i = 0; i < 6; i++) {
+    aray[i] = regenboog[i];
+    console.log(aray[i]);
+}
+while (aray.length >= 1){
+  console.log(aray.pop())  
+}
