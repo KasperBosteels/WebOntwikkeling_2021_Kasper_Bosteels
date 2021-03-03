e6c7b4194330be14bd5a815a1f090b3545165965
KasperBosteels <kasperbosteels@hotmail.com>
2021-03-03 11:50:18 +0100

    oefening 3 gedaan

---
+++ labo2/oefening3.ts
@@ -0,0 +1,9 @@
+let arr = [];
+for (let i = 0; i <= 10; i++) {
+    arr[i] = 7*i;
+    if(arr[i] % 3 == 0 && arr[i] % 5 == 0){
+    }else{
+        if(arr[i] % 3 == 0 || arr[i] % 5 == 0){console.log(arr[i]);
+    }
+    }
+}
