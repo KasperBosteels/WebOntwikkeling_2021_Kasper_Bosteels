1e7dfab994e77f6f877bff27d7aa9150a0b87c87
KasperBosteels <kasperbosteels@hotmail.com>
2021-03-03 10:24:46 +0100

    labo 1 gedaan

---
+++ labo1/PrintNameAndAge.ts
@@ -0,0 +1,19 @@
+
+
+
+
+
+let Naam,Voornaam,Leeftijd,Zin;
+Naam = "Bosteels";
+Voornaam = "Kasper"
+Leeftijd = 23;
+Zin = `ik ben ${Voornaam} ${Naam} en ik ben ${Leeftijd} jaar oud.`;
+console.log(Zin);
+
+
+
+
+
+
+
+
