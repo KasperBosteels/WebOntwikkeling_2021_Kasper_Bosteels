90c9fdabe27a4a68fc578a4ac391ef6339a0432e
KasperBosteels <kasperbosteels@hotmail.com>
2021-03-03 11:50:27 +0100

    oefening 4 gedaan

---
+++ labo2/oefening4.ts
@@ -0,0 +1,12 @@
+let ar = [];
+for (let i = 0; i <= 100; i++) {
+ar[i] = i;
+if(ar[i] % 3 == 0){
+    ar[i] = "fizz";
+}else if (ar[i] % 5 == 0){
+    ar[i] = "buzz"
+}
+}
+for (let y = 0; y < ar.length; y++) {
+console.log(ar[y]);
+}
