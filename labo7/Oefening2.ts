c3ea7e4d0e1e88be33a99d2d27ea7fc06456169d
KasperBosteels <kasperbosteels@hotmail.com>
2021-04-23 18:18:47 +0200

    labo 7 Oefening 2 klaar

---
+++ labo7/Oefening2.ts
@@ -0,0 +1,17 @@
+const Fetch = require('node-fetch');
+
+const getCoctail = async (id:number):Promise<string> =>{
+    let result:any = await Fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    let response:any = await result.json();
+    return response.drinks[0].strDrink;
+}
+getCoctail(11000).then(name =>{
+    console.log(name);
+});
+getCoctail(11001).then(name=>{
+    console.log(name);
+});
+getCoctail(11002).then(name =>{
+    console.log(name);
+});
+
