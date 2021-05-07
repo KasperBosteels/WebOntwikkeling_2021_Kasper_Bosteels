7f21b0738df7b2256d1737550def8ac1f5d46011
KasperBosteels <kasperbosteels@hotmail.com>
2021-05-07 20:56:04 +0200

    labo 10 klaar

--- labo9/index.ts
+++ labo9/index.ts
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
+app.set('view engine', 'ejs');
 app.set('port',3000);
-
-
+app.use(express.static(__dirname+'/static'));
 
 
 app.get('/',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('<p>Hello World</p>');
+    res.render('./landing')
+})
+
+app.get('/movies',(req:any,res:any)=>{
+    res.render('./movies')
+})
+
+app.get('/movie/:index',(req:any,res:any)=>{
+    let index = req.params.index;
+    res.render('./movie',{
+        number: index
     })
+})
 
 
 
